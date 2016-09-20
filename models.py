40b34e4dc4baad23c1218b53743a9769e1d8c224
tracie-russell <ivorygirl@gmail.com>
2016-09-19 19:49:47 -0500

    final commit

--- models.py
+++ models.py
@@ -5,8 +5,10 @@ class User(db.Model):
     pw_hash = db.StringProperty(required = True)
     email = db.StringProperty()
 
+
 class Post(db.Model):
     title = db.StringProperty(required = True)
     body = db.TextProperty(required = True)
     created = db.DateTimeProperty(auto_now_add = True)
+    author = db.ReferenceProperty(User, required = True)
     # TODO - we need an author field here; it should be required
